03f31d531dab1cacc5555a411cbb4283987bc646
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-04-05 15:42:08 +0100

    Initial version of Chunk class (#2)

---
+++ proj1/src/Chunk.java
@@ -0,0 +1,20 @@
+import java.io.Serializable;
+
+class Chunk implements Serializable {
+    private String fileID;
+    private Integer chunkID;
+    private Integer replicationDegree;
+    private byte[] data;
+    
+    Chunk (String fileID, Integer chunkID, Integer replicationDegree, byte[] data) {
+        this.fileID = fileID;
+        this.chunkID = chunkID;
+        this.replicationDegree = replicationDegree;
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chunk <%d> , FileID: <%s> , ReplicationDegree: <%d>", this.chunkID, this.fileID, this.replicationDegree);
+    }
+}
