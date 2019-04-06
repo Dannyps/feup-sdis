37587003ec2d402068e96293ed400e267f05308c
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-04-06 18:54:48 +0100

    Add method to create the message header (#5)

--- proj1/src/Messages/Message.java
+++ proj1/src/Messages/Message.java
@@ -1,5 +1,7 @@
 package Messages;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public abstract class Message {
@@ -10,12 +12,13 @@ public abstract class Message {
 
     /** Optional fields */
     protected byte[] fileId = null;
-    protected Integer chunkNo = -1;
-    protected Integer replicationDegree = -1;
+    protected Integer chunkNo = null;
+    protected Integer replicationDegree = null;
 
     /**
-     * Converts a Java string to an array of bytes. Each byte represents a string character in 7-bit ASCII format
-     * Note: Byte values are signed
+     * Converts a Java string to an array of bytes. Each byte represents a string
+     * character in 7-bit ASCII format Note: Byte values are signed
+     * 
      * @param str
      * @return
      */
@@ -38,31 +41,32 @@ public abstract class Message {
     };
 
     /**
-     * @return Returns the server/sender/peer identifier in ASCII bytes 
+     * @return Returns the server/sender/peer identifier in ASCII bytes
      */
     protected byte[] getSenderIdASCII_() {
         return this.stringToASCII_(this.senderId.toString());
     };
-    
+
     /**
-     * @return Returns the file identifier such that each hash byte is represented by two ASCII bytes
+     * @return Returns the file identifier such that each hash byte is represented
+     *         by two ASCII bytes
      */
     protected byte[] getFileIdASCII_() {
 
         byte[] b = new byte[64];
         for (int i = 0; i < this.fileId.length; i++) {
             byte[] aux = Integer.toHexString(0xff & this.fileId[i]).getBytes(StandardCharsets.US_ASCII);
-            int j = i*2;
-            if(aux.length == 1) {
+            int j = i * 2;
+            if (aux.length == 1) {
                 b[j] = '0';
-                b[j+1] = aux[0];
+                b[j + 1] = aux[0];
             } else {
                 b[j] = aux[0];
-                b[j+1] = aux[1];
+                b[j + 1] = aux[1];
             }
         }
-        //String s = new String(b, StandardCharsets.US_ASCII);
-        //System.out.println(s);
+        // String s = new String(b, StandardCharsets.US_ASCII);
+        // System.out.println(s);
         return b;
     }
 
@@ -80,6 +84,62 @@ public abstract class Message {
         return this.stringToASCII_(this.replicationDegree.toString());
     }
 
+    /**
+     * @return Returns the <CRLF> terminator sequence
+     */
+    protected byte[] getSequenceTerminator() {
+        return new byte[]{'\r', '\n'};
+    }
+
+    /**
+     * Creates the header following the protocol specification. In addition to the mandatory/common fields between sub-protocols, it considers aditional not-null fields. It assumes the following sequence:
+     * <Message type> <Version> <SenderId> <FileId> <ChunkNo> <ReplDegree> <CRLF><CRLF>
+     * 
+     * @return
+     */
+    protected byte[] createHeader() {
+        // Dynamic byte buffer
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        
+        // Generic format
+        // <Message type> <Version> <SenderId> <FileId> <ChunkNo> <ReplDegree> <CRLF><CRLF>
+        
+        try {
+            // Mandatory fields
+            buf.write(this.getMsgTypeASCII_());
+            buf.write(' ');
+
+            buf.write(this.getVersionASCII_());
+            buf.write(' ');
+            
+            buf.write(this.getSenderIdASCII_());
+            buf.write(' ');
+            
+            buf.write(this.getFileIdASCII_());
+            buf.write(' ');
+
+            // Subprotocol required fields
+            if(this.chunkNo != null) {
+                buf.write(this.getChunkNoASCII_());
+                buf.write(' ');
+            }
+
+            if(this.replicationDegree != null) {
+                buf.write(this.getChunkNoASCII_());
+                buf.write(' ');
+            }
+
+            // Terminator sequence
+            buf.write(this.getSequenceTerminator());
+            buf.write(' ');
+            buf.write(this.getSequenceTerminator());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return buf.toByteArray();
+    }
+    
     /**
      * 
      * @param type
