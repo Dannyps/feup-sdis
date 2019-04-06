ab0ed4d81abff111b354b87f10d5715f58d03012
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-04-06 14:16:27 +0100

    Print chunk's file identifier as hexadecimal hashes (#7)

--- proj1/src/Chunk.java
+++ proj1/src/Chunk.java
@@ -13,8 +13,24 @@ class Chunk implements Serializable {
         this.data = data;
     }
 
+    /**
+     * Builds an hexadecimal string representation of the file hash
+     * @return File identifier in hexadecimal format
+     */
+    private String getHexHash() {
+        StringBuffer hexString = new StringBuffer();
+
+        for (int i = 0; i < this.fileID.length; i++) {
+            String hex = Integer.toHexString(0xff & this.fileID[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+
     @Override
     public String toString() {
-        return String.format("Chunk <%d>, FileID: <%s>, ReplicationDegree: <%d>, Size: <%d>", this.chunkID, this.fileID, this.replicationDegree, this.data.length);
+        return String.format("FileID: <%s>, Chunk <%d>, ReplicationDegree: <%d>, Size: <%d>", this.getHexHash(), this.chunkID, this.replicationDegree, this.data.length);
     }
 }
