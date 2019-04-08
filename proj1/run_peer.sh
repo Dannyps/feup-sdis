c2d47201fc304782f4f43a760edcb4d6430c012d
Daniel Silva <mail@dannyps.net>
2019-04-08 09:22:18 +0100

    Make it possible to pass peer name on run_peer.sh

--- proj1/run_peer.sh
+++ proj1/run_peer.sh
@@ -6,8 +6,14 @@ MCR_ADDR="localhost:8003"
 # Define other parameters
 PROTOCOL_VERSION="1.0"
 PEER_ID=1 # todo, allow incremental peer launching
-PEER_AP="peer_1"
+
+if [ -z "$1" ]
+then
+    PEER_AP="peer_1"
+else
+    PEER_AP=$1
+fi
 
 # Launch peer
 ARG_LIST="$MC_ADDR $MCB_ADDR $MCR_ADDR $PROTOCOL_VERSION $PEER_ID $PEER_AP"
-java -cp "bin/" Peer $ARG_LIST 
+java -cp "bin/" Peer $ARG_LIST
