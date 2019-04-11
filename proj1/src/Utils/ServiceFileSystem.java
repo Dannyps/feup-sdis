fc0541f3b8be51ec56065d225cb960c6f6242e6b
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-04-11 23:17:28 +0100

    Add class to manage relevant resources locations (chunks and eventually other content)

---
+++ proj1/src/Utils/ServiceFileSystem.java
@@ -0,0 +1,42 @@
+package Utils;
+
+import Shared.Peer;
+
+/**
+ * Collection of static methods that tell the specific location for storing files and chunks data
+ */
+public class ServiceFileSystem {
+    /**
+     * @param fileId The file id hash string in hexadecimal representation
+     * @return The relative path to the backup file directory
+     */
+    public static String getBackupFilePath(String fileId) {
+        return String.format("peer%d/backup/%s", Peer.getInstance().getPeerId(), fileId);
+    }
+
+    /**
+     * @param fileId The file id hash string in hexadecimal representation
+     * @param chunkNo The chunk number
+     * @return The relative path to the backup chunk of the file fileId
+     */
+    public static String getBackupChunkPath(String fileId, Integer chunkNo) {
+        return String.format("peer%d/backup/%s/chk%d", Peer.getInstance().getPeerId(), fileId, chunkNo);
+    }
+
+    /**
+     * @param fileId The file id hash string in hexadecimal representation
+     * @return The relative path to the restored file directory
+     */
+    public static String getRestoredFilePath(String fileId) {
+        return String.format("peer%d/restore/%s", Peer.getInstance().getPeerId(), fileId);
+    }
+
+    /**
+     * @param fileId The file id hash string in hexadecimal representation
+     * @param chunkNo The chunk number
+     * @return The relative path to the restored chunk of the file fileId
+     */
+    public static String getRestoredChunkPath(String fileId, Integer chunkNo) {
+        return String.format("peer%d/restore/%s/chk%d", Peer.getInstance().getPeerId(), fileId, chunkNo);
+    }
+}
