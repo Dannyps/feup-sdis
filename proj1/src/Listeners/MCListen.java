4076e89e6dd8146807d7ea30d2f181d9985dd910
Fábio Gaspar <fabiodanielreis@hotmail.com>
2019-04-13 15:18:03 +0100

    Proccess delete messages on MC channel (#18)

--- proj1/src/Listeners/MCListen.java
+++ proj1/src/Listeners/MCListen.java
@@ -8,12 +8,14 @@ import Messages.MessageType;
 import Shared.Peer;
 import Utils.PrintMessage;
 import Workers.ChunkSenderWorker;
+import Workers.DeleteSenderWorker;
+import Workers.DeleteWorker;
 
 /**
  * MCListen
  */
 public class MCListen extends ChannelListener {
-    
+
     /**
      * 
      * @param s Multicast channel for control messages
@@ -25,12 +27,15 @@ public class MCListen extends ChannelListener {
     @Override
     protected void newMessageHandler(Message msg) {
         PrintMessage.p("Received", msg);
-        if(msg.getMessageType() == MessageType.STORED) {
+        if (msg.getMessageType() == MessageType.STORED) {
             Peer.getInstance().chunkStored(msg.getFileIdHexStr(), msg.getChunkNo(), msg.getSenderId());
-        }else if(msg.getMessageType() == MessageType.GETCHUNK && msg.getSenderId() != this.serverId){
+        } else if (msg.getMessageType() == MessageType.GETCHUNK && msg.getSenderId() != this.serverId) {
             // launch thread to reply requested chunk
             ChunkSenderWorker w = new ChunkSenderWorker((GetChunkMessage) msg);
             executor.submit(w);
+        } else if (msg.getMessageType() == MessageType.DELETE && msg.getSenderId() != this.serverId) {
+            // launch thread to process the message and delete chunks of the said file
+            executor.submit(new DeleteWorker(msg.getFileId()));
         }
     }
 }
